7627972b1740d35a1a5bdf92f4c07d66248e4e0a
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2023-11-29 19:13:43 -0300

    :wrench: chore: NestJS configurado para suportar WebSocket

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+// import { Controller, Get } from '@nestjs/common';
+// import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+
+@WebSocketGateway(3001) // Porta para WebSocket
+export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server;
+
+  handleConnection(client) {
+    console.log(`Cliente conectado: ${client.id}`);
+  }
+
+  handleDisconnect(client) {
+    console.log(`Cliente desconectado: ${client.id}`);
   }
 }
