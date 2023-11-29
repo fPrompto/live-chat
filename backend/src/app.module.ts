3e3fd9bdd25c02c9bb8a607a24764c7cd3a2a61a
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2023-11-29 18:30:50 -0300

    :wrench: chore: Configuração do Módulo e Service do Prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
-export class AppModule {}
+export class PrismaModule {}
