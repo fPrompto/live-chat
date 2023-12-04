cf9d3ca02e3691dabf9a7fd655fb06004bd2c4e0
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2023-12-04 15:02:34 -0300

    :sparkles: feat: ContextAPI adicionado a aplicação

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 // pages/_app.js
 import { ChakraProvider } from '@chakra-ui/react';
+import ChatProvider from '@/context/ChatProvider';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ChatProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ChatProvider>
   );
 }
 
